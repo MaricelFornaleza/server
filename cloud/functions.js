80908fdf4a1a42a948f77dc7a47c591d5dc66a53
Maricel Fornaleza <maformaleza@gbox.adnu.edu.ph>
2022-05-13 21:07:30 +0800

    first commit

---
+++ cloud/functions.js
@@ -0,0 +1,112 @@
+Parse.Cloud.define('hello', req => {
+  req.log.info(req);
+  return 'Hi';
+});
+
+Parse.Cloud.define('asyncFunction', async req => {
+  await new Promise(resolve => setTimeout(resolve, 1000));
+  req.log.info(req);
+  return 'Hi async';
+});
+
+Parse.Cloud.beforeSave('Test', () => {
+  throw new Parse.Error(9001, 'Saving test objects is not available.');
+});
+
+const nodemailer = require("nodemailer");
+const hbs = require('nodemailer-express-handlebars')
+const path = require('path')
+const transporterOptions = {host: "smtp.gmail.com",
+port: 587,
+secure: false, // true for 465, false for other ports
+auth: {
+  user: 'chednstpserialnumber@gmail.com',
+  pass: 'chednstpserialnumber2022',
+  },
+}
+
+async function sendEmail(params = {}) {
+  let transporter = nodemailer.createTransport(transporterOptions);
+  // point to the template folder
+const handlebarOptions = {
+  viewEngine: {
+      partialsDir: path.resolve('./views/'),
+      defaultLayout: false,
+  },
+  viewPath: path.resolve('./views/'),
+};
+
+  // use a template file with nodemailer
+transporter.use('compile', hbs(handlebarOptions))
+  // send mail with defined transport object
+  if (params.type == "Account") {
+    let info = await transporter.sendMail({
+      from: '"CHED NSTP Serial Number" <chednstpserialnumber@gmail.com>', // sender address
+      to: params.email, // list of receivers
+      subject: "Account Credentials", // Subject line
+      template: 'credential',
+      context:{
+        name: params.name,
+        password: params.password
+      }
+    });
+    
+  }
+
+  if (params.type == "Contact") {
+    let info = await transporter.sendMail({
+      from: '"CHEDRO V NSTP" <chednstpserialnumber@gmail.com>', // sender address
+      to: 'lykaccasilao@gmail.com', // list of receivers
+      subject: "Message", // Subject line
+      text: params.message, // plain text body
+    });
+  }
+
+  if (params.type == "Notification") {
+    let info = await transporter.sendMail({
+      from: '"CHED NSTP Serial Number" <chednstpserialnumber@gmail.com>', // sender address
+      to: params.email,
+      subject: "Application Update: " + params.title,
+      template: 'notification',
+      context: {
+        name: params.name,
+        document: params.document,
+        date: params.date
+
+      }
+    })
+  }
+  if (params.type == "Transmittal") {
+    let info = await transporter.sendMail({
+      from: '"CHED NSTP Serial Number" <chednstpserialnumber@gmail.com>', // sender address
+      to: params.email,
+      subject: "Application Update: " + params.title,
+      template: 'letter',
+      context: {
+        name: params.name,
+        dataApplied: params.dataApplied,
+        schoolYear: params.schoolYear,
+        serialNumber: params.serialNumber,
+        noOfStudents: params.noOfStudents
+      }
+    })
+  }
+
+  console.log("Message sent: %s", info.messageId);
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  
+
+  // console.log("Message sent: %s", info.messageId);
+  // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+}
+const approved = true;
+Parse.Cloud.define("sendEmailNotification", async (request) => {
+  if (approved) {
+    sendEmail(request.params).then(function () {
+      return 'ok'
+    }, (error) => {
+      console.log(error)
+      throw new Error(error)
+    })
+  }
+});
---
+++ index.js
@@ -0,0 +1,94 @@
+// Example express application adding the parse-server module to expose Parse
+// compatible API routes.
+
+const express = require('express');
+const ParseServer = require('parse-server').ParseServer;
+const path = require('path');
+const args = process.argv || [];
+const test = args.some(arg => arg.includes('jasmine'));
+
+const databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
+
+if (!databaseUri) {
+  console.log('DATABASE_URI not specified, falling back to localhost.');
+}
+const config = {
+  databaseURI: databaseUri || 'mongodb://localhost:36463/nstpdb',
+  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+  appId: process.env.APP_ID || 'myAppId',
+  masterKey: process.env.MASTER_KEY || 'masterKey', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'http://localhost:1337/api', // Don't forget to change to https if needed
+  liveQuery: {
+    classNames: ['_User', 'Application', 'NstpEnrollment'], // List of classes to support for query subscriptions
+  },
+  javascriptKey: "jsKey",
+  accountLockout: {
+    duration: 5,
+    threshold: 3,
+    unlockOnPasswordreset:true,
+  },
+  passwordPolicy: {
+    doNotAllowUsername: true,
+    maxPasswordHistory: 5
+  },
+  preventLoginWithUnverifiedEmail: true,
+  enableAnonymousUsers: false,
+  verifyUserEmails: true,
+  emailVerifyTokenValidityDuration: 12 * 60 * 60,
+  appName: 'ched',
+  publicServerURL: 'http://localhost:1337/api',
+  emailAdapter: {
+    module: 'parse-smtp-template',
+    options: {
+      secure: true,
+      port: 465,
+      host: 'smtp.gmail.com',
+      user: 'chednstpserialnumber@gmail.com',
+      password: 'chednstpserialnumber2022',
+      fromAddress: 'CHEDRO V NSTP <chednstpserialnumber@gmail.com>',
+	
+	}
+  }
+
+};
+// Client-keys like the javascript key or the .NET key are not necessary with parse-server
+// If you wish you require them, you can set them as options in the initialization above:
+// javascriptKey, restAPIKey, dotNetKey, clientKey
+
+const app = express();
+
+// Serve static assets from the /public folder
+app.use('/', express.static(path.join(__dirname, '/public')));
+
+// Serve the Parse API on the /parse URL prefix
+const mountPath = process.env.PARSE_MOUNT || '/api';
+if (!test) {
+  const api = new ParseServer(config);
+  app.use(mountPath, api);
+}
+
+// // Parse Server plays nicely with the rest of your web routes
+// app.get('/', function (req, res) {
+//   res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+// });
+
+// // There will be a test page available on the /test path of your server url
+// // Remove this before launching your app
+// app.get('/test', function (req, res) {
+//   res.sendFile(path.join(__dirname, '/public/test.html'));
+// });
+
+const port = process.env.PORT || 1337;
+if (!test) {
+  const httpServer = require('http').createServer(app);
+  httpServer.listen(port, function () {
+    console.log('parse-server-example running on port ' + port + '.');
+  });
+  // This will enable the Live Query real-time server
+  ParseServer.createLiveQueryServer(httpServer);
+}
+
+module.exports = {
+  app,
+  config,
+};
