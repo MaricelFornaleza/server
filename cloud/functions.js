fa6101850cba3c045c07547cd3ae69099d127856
Lyka Casilao <lycasilao/2gbox.adnu.edu.ph>
2022-05-17 22:00:46 +0800

    lyka server

--- cloud/functions.js
+++ cloud/functions.js
@@ -1,17 +1,5 @@
-<<<<<<< HEAD
-var admin = require('firebase-admin'); 
 
-var serviceAccount = require(__dirname + '/nsnoais-firebase-adminsdk-lfea3-7b05ec1c42.json')
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-})
-
-=======
->>>>>>> 75208e989cd7f2526b3f80afcf897c6c782af4b0
 Parse.Cloud.define('hello', req => {
-  req.log.info(req);
-  return 'Hi';
 });
 
 Parse.Cloud.define('asyncFunction', async req => {
@@ -25,93 +13,6 @@ Parse.Cloud.beforeSave('Test', () => {
 });
 
 const nodemailer = require("nodemailer");
-<<<<<<< HEAD
-
-async function sendEmail(params = {}) {
-  let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: 'chednstpserialnumber@gmail.com',
-      pass: 'chednstpserialnumber2022',
-    },
-  });
-
-  // send mail with defined transport object
-  if (params.type == "Account") {
-    let info = await transporter.sendMail({
-      from: '"CHEDRO V NSTP" <chednstpserialnumber@gmail.com>', // sender address
-      to: params.email, // list of receivers
-      subject: "Account Credential", // Subject line
-      text: "Email:" + params.email + "\n Password: " + params.password, // plain text body
-    }); 
-  }
-  if (params.type == "Contact") {
-    let info = await transporter.sendMail({
-      from: '"CHEDRO V NSTP" <chednstpserialnumber@gmail.com>', // sender address
-      to: 'lykaccasilao@gmail.com', // list of receivers
-      subject: "Message", // Subject line
-      text: params.message, // plain text body
-    });
-  }
-
-  console.log("Message sent: %s", info.messageId);
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-}
- 
-Parse.Cloud.define("accountCredential", async (request) => {
-if (params.approved) {
-sendEmail(request.params).then(function () {
-    return 'ok'
-  }, (error) => {
-    console.log(error)
-    throw new Error(error)
-  })
-}
-});
-
-Parse.Cloud.define("contactForm", async (request) => {
-if (params.approved) {
-sendEmail(request.params).then(function () {
-  return 'ok'
-}, (error) => {
-  console.log(error)
-  throw new Error(error)
-})
-}
-});
-
-Parse.Cloud.define('hello', req => {
-  // Parse.Cloud.run('sendFCMNotification', {
-  //   title: 'Say hi',
-  // })
-  req.log.info(req);
-  return 'Hi';
-});
-
-Parse.Cloud.define('sendFMCNotification', async (req) => {
-  const { title, body, tokens, link } = req.params
-
-  if (!title) throw new Error("Title is required");
-  if (!body) throw new Error("Body is required");
-  if (tokens.length == 0) throw new Error('Tokens are required');
-
-  let message = {
-    notification: {
-      title: title,
-      body: body,
-      click_action: 'https://localhost:8080'
-    }
-  }
-
-  if (link) message.notification.click_action = link
-  try {
-    const { failureCount, successCount } = await admin.messaging().sendToDevice(token, message, {priority: high});
-    console.log('Successfully send the notification to ${successCount} device (${failureCount} failed')
-  }catch (err) {
-    console.log('An error occured while connecting to Firebase')
-=======
 const hbs = require('nodemailer-express-handlebars')
 const path = require('path')
 const transporterOptions = {host: "smtp.gmail.com",
@@ -214,6 +115,5 @@ Parse.Cloud.define("sendEmailNotification", async (request) => {
       console.log(error)
       throw new Error(error)
     })
->>>>>>> 75208e989cd7f2526b3f80afcf897c6c782af4b0
   }
 });
--- index.js
+++ index.js
@@ -13,17 +13,12 @@ if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 const config = {
-<<<<<<< HEAD
   databaseURI: databaseUri || 'mongodb://localhost:36463/test',
-=======
-  databaseURI: databaseUri || 'mongodb://localhost:36463/nstpdb',
->>>>>>> 75208e989cd7f2526b3f80afcf897c6c782af4b0
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || 'masterKey', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/api', // Don't forget to change to https if needed
   liveQuery: {
-<<<<<<< HEAD
     classNames: ['_User', 'Application', 'NstpEnrollment', 'Notification'], // List of classes to support for query subscriptions
   },
   javascriptKey: 'jsKey',
@@ -35,19 +30,6 @@ const config = {
   passwordPolicy: {
     doNotAllowUsername: true,
     masPasswordHistory: 5,
-=======
-    classNames: ['_User', 'Application', 'NstpEnrollment'], // List of classes to support for query subscriptions
-  },
-  javascriptKey: "jsKey",
-  accountLockout: {
-    duration: 5,
-    threshold: 3,
-    unlockOnPasswordreset:true,
-  },
-  passwordPolicy: {
-    doNotAllowUsername: true,
-    maxPasswordHistory: 5
->>>>>>> 75208e989cd7f2526b3f80afcf897c6c782af4b0
   },
   preventLoginWithUnverifiedEmail: true,
   enableAnonymousUsers: false,
@@ -58,21 +40,13 @@ const config = {
   emailAdapter: {
     module: 'parse-smtp-template',
     options: {
-<<<<<<< HEAD
-=======
       template: true,
       templatePath: "views/template.html",
->>>>>>> 75208e989cd7f2526b3f80afcf897c6c782af4b0
       secure: true,
       port: 465,
       host: 'smtp.gmail.com',
       user: 'chednstpserialnumber@gmail.com',
       password: 'chednstpserialnumber2022',
-<<<<<<< HEAD
-      fromAddress: 'ched <chednstpserialnumber@gmail.com>',
-    }
-  }
-=======
       fromAddress: 'CHEDRO V NSTP <chednstpserialnumber@gmail.com>',
       confirmOptions: {
         subject: "Email Verification",
@@ -83,7 +57,6 @@ const config = {
 	}
   }
 
->>>>>>> 75208e989cd7f2526b3f80afcf897c6c782af4b0
 };
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
@@ -96,37 +69,21 @@ app.use('/', express.static(path.join(__dirname, '/public')));
 
 // Serve the Parse API on the /parse URL prefix
 const mountPath = process.env.PARSE_MOUNT || '/api';
-<<<<<<< HEAD
-  if (!test) {
-=======
 if (!test) {
->>>>>>> 75208e989cd7f2526b3f80afcf897c6c782af4b0
   const api = new ParseServer(config);
   app.use(mountPath, api);
 }
 
-<<<<<<< HEAD
-// Parse Server plays nicely with the rest of your web routes
-=======
 // // Parse Server plays nicely with the rest of your web routes
->>>>>>> 75208e989cd7f2526b3f80afcf897c6c782af4b0
 // app.get('/', function (req, res) {
 //   res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
 // });
 
-<<<<<<< HEAD
-// There will be a test page available on the /test path of your server url
-// Remove this before launching your app
-app.get('/test', function (req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
-});
-=======
 // // There will be a test page available on the /test path of your server url
 // // Remove this before launching your app
 // app.get('/test', function (req, res) {
 //   res.sendFile(path.join(__dirname, '/public/test.html'));
 // });
->>>>>>> 75208e989cd7f2526b3f80afcf897c6c782af4b0
 
 const port = process.env.PORT || 1337;
 if (!test) {
